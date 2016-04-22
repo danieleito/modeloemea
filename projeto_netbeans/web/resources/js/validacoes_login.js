244408c56defa65e6f1db330fc5247c61d16c03e
Daniele Harumi Ito <ito.daniele@gmail.com>
2016-04-22 14:48:20 -0300

    Adicionei arquivos css.

---
+++ projeto_netbeans/web/resources/js/validacoes_login.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function validar() {
+    var usuario = document.getElementById("j_idt9:usuario").value;
+    if (!usuario) {
+        alert('Campo usuário é obrigatório.');
+        return false;
+    }
+    var senha = document.getElementById("j_idt9:senha").value;
+    if (!senha) {
+        alert('Campo senha é obrigatório.');
+        return false;
+    }
+    var novaSenha = document.getElementById("j_idt9:novaSenha").value;
+    var confirmarNovaSenha = document.getElementById("j_idt9:confirmarNovaSenha").value;
+    if (novaSenha && !confirmarNovaSenha) {
+        alert('Campo confirmar nova senha é obrigatório para alterar senha.');
+        return false;
+    }
+    if (novaSenha && confirmarNovaSenha && novaSenha !== confirmarNovaSenha) {
+        alert('Campo nova senha e confirmar nova senha não conferem.');
+        return false;
+    }
+}
