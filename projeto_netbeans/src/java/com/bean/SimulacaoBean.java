87f9e8c2b19c88547a01176cb84f0a2d9678b385
Daniele Harumi Ito <ito.daniele@gmail.com>
2016-04-25 19:34:30 -0300

    Correção da validação no cadastro de novas simulações.

--- projeto_netbeans/src/java/com/bean/SimulacaoBean.java
+++ projeto_netbeans/src/java/com/bean/SimulacaoBean.java
@@ -60,9 +60,13 @@ public class SimulacaoBean extends ComumBean {
     
     public void salvar() {
         try {
-            database.inserir(novaSimulacao);
-            simulacoes = database.buscar();
-            adicionarMensagemInfo("Simulação cadastrada com sucesso.");
+            if (novaSimulacao.getSimulacao() == null || novaSimulacao.getSimulacao().isEmpty()) {
+                adicionarMensagemErro("Campo 'Nome da simulação' é obrigatório.");
+            } else {
+                database.inserir(novaSimulacao);
+                simulacoes = database.buscar();
+                adicionarMensagemInfo("Simulação cadastrada com sucesso.");
+            }
         } catch (SQLException ex) {
             Logger.getLogger(SimulacaoBean.class.getName()).log(Level.SEVERE, null, ex);
             adicionarMensagemErro("Erro ao cadastrar simulação: " + ex.getMessage());
